9410fc88bd550ddac509d0c9319c29bcaa22f10d
Ced M <mouneac@gmail.com>
2022-03-08 20:06:01 -0600

    03/08/2022  08:06PM Being a while. Little progress made today This is harder than expected I have a basic interface. Just need to make the code dynamic (i.e the game should play on player selection)

--- game.js
+++ game.js
@@ -1,4 +1,18 @@
-// Computer function which return a hand sign.
+
+// Below I am trying to have the hand selection show on the webpage
+
+let yourChoice = document.getElementById("yourChoice");
+let computerChoice = document.getElementById("computerChoice");
+let result = document.getElementById("result");
+let allChoices = document.querySelectorAll("button");
+let userChoice;
+
+allChoices.forEach(allChoices => allChoices.addEventListener("click", (e)=> {
+    userChoice = e.target.id 
+    yourChoice.innerHTML = userChoice
+}))
+
+// Computer function which return a hand sign randomly.
 
 function computerPlay(){
     let hands = ['Rock', 'Paper', 'Scissors'];
@@ -23,14 +37,32 @@ function playerPlay(){
 
 function letPlay (playerSelection, computerSelection){
     playerSelection = playerPlay();
+    // playerSelection = yourChoice;
     computerSelection = computerPlay();
     let matchCheck = `${playerSelection} vs ${computerSelection}`;
 
     if (playerSelection === computerSelection) {
-        // alert(`${matchCheck} is a tie`);
+
         return `${matchCheck} is a tie`;
-    }
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return `${matchCheck} You won! Rock breaks Scissors`;
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return `${matchCheck} You won! Paper wraps Rock`;
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return `${matchCheck} You won! Scissors cut Paper`;
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return `${matchCheck} You Lose! Scissors cut Paper`;
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return `${matchCheck} You Lose! Paper wraps Rock`;
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return `${matchCheck} You Lose! Rock breaks Paper`;
+    } 
 }
+
+computerChoice.innerHTML = computerPlay();
     
 
-console.log(letPlay());
+console.log(letPlay()); //Function works
+
+
+
